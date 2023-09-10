2304705b41ab69531241fa67da4540a7e54adb39
Azhar <darkcoders0101@gmail.com>
2023-09-10 15:01:30 +0530

    upadating css, js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,17 @@ if (selectedTheme) {
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true, // Animation repeat
+})
+
+sr.reveal('.home__data, .join__container')
+sr.reveal('.home__img, .footer', {origin: 'bottom'})
+sr.reveal('.enjoy__card, .popular__card', {interval: 100})
+sr.reveal('.about__data', {origin: 'right'})
+sr.reveal('.about__img', {origin: 'left'})
